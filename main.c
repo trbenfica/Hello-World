85712bef6a59cd5821ab1e04cf74047cba9c6cd3
Tomás Benfica <tomas_r_benfica@hotmail.com>
2020-06-20 11:43:54 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+// Demonstration of the use of pointers in C.
+// through pointers, we will be able to change
+// the value of two variables through a function.
+
 #include <stdio.h>
 
-int main()
+void switchArgs (int* a, int* b)
 {
-  printf("\nHey there\n");
+  int tmp;
+  tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int main ()
+{
+  int a = 5;
+  int b = 8;
+  printf ("\nBefore the switch: a = %d, b = %d\n", a, b);
+  switchArgs (&a, &b);
+  printf ("\nAfter the switch: a = %d, b = %d\n", a, b);
   return 0;
 }
